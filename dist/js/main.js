852b256c8401b7f2e1f1be82bb2680f8fa042788
jamiebdavis <jamied1985@gmail.com>
2019-08-10 09:33:18 +0100

    added scss for menu button transition

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,37 @@
 // Select DOM items
 
-const menuBtn = document.querySelector(".btn-menu");
+const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuBranding = document.querySelector(".menu-branding");
 const menuNav = document.querySelector(".menu-nav");
 
-const navItems = document.querySelectorAll(".nav-items");
+const navItems = document.querySelectorAll(".nav-item");
 
 //  Set intial state of menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+
+    navItems.forEach(item => item.classList.add("show"));
+
+    //   Show menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+
+    navItems.forEach(item => item.classList.remove("show"));
+
+    //   Show menu state
+    showMenu = false;
+  }
+}
